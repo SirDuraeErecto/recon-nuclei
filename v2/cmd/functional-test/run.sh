cb3ee281625ce56d82eb2195a5e2e92f8d45ce91
Paul Wensing <SirDuraeErecto@pm.me>
2022-02-14 01:18:51 -0500

    Initial commit

---
+++ v2/cmd/functional-test/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# reading os type from arguments
+CURRENT_OS=$1
+
+if [ "${CURRENT_OS}" == "windows-latest" ];then
+    extension=.exe
+fi
+
+echo "::group::Building functional-test binary"
+go build -o functional-test$extension
+echo "::endgroup::"
+
+echo "::group::Building Nuclei binary from current branch"
+go build -o nuclei_dev$extension ../nuclei
+echo "::endgroup::"
+
+echo "::group::Building latest release of nuclei"
+go build -o nuclei$extension -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+echo "::endgroup::"
+
+echo 'Starting Nuclei functional test'
+./functional-test$extension -main ./nuclei$extension -dev ./nuclei_dev$extension -testcases testcases.txt
