f33572bd1fbf1848b2f31d1e8f99ebcd8712097a
Paul Wensing <SirDuraeErecto@pm.me>
2022-02-14 01:19:08 -0500

    Initialize interact-debug

---
+++ v2/pkg/protocols/headless/request.go
@@ -0,0 +1,88 @@
+package headless
+
+import (
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/nuclei/v2/pkg/output"
+	"github.com/projectdiscovery/nuclei/v2/pkg/protocols"
+	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/helpers/eventcreator"
+	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/helpers/responsehighlighter"
+	templateTypes "github.com/projectdiscovery/nuclei/v2/pkg/templates/types"
+)
+
+var _ protocols.Request = &Request{}
+
+// Type returns the type of the protocol request
+func (request *Request) Type() templateTypes.ProtocolType {
+	return templateTypes.HeadlessProtocol
+}
+
+// ExecuteWithResults executes the protocol requests and returns results instead of writing them.
+func (request *Request) ExecuteWithResults(inputURL string, metadata, previous output.InternalEvent /*TODO review unused parameter*/, callback protocols.OutputEventCallback) error {
+	instance, err := request.options.Browser.NewInstance()
+	if err != nil {
+		request.options.Output.Request(request.options.TemplatePath, inputURL, request.Type().String(), err)
+		request.options.Progress.IncrementFailedRequestsBy(1)
+		return errors.Wrap(err, "could get html element")
+	}
+	defer instance.Close()
+
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		request.options.Output.Request(request.options.TemplatePath, inputURL, request.Type().String(), err)
+		request.options.Progress.IncrementFailedRequestsBy(1)
+		return errors.Wrap(err, "could get html element")
+	}
+	out, page, err := instance.Run(parsedURL, request.Steps, time.Duration(request.options.Options.PageTimeout)*time.Second)
+	if err != nil {
+		request.options.Output.Request(request.options.TemplatePath, inputURL, request.Type().String(), err)
+		request.options.Progress.IncrementFailedRequestsBy(1)
+		return errors.Wrap(err, "could get html element")
+	}
+	defer page.Close()
+
+	request.options.Output.Request(request.options.TemplatePath, inputURL, request.Type().String(), nil)
+	request.options.Progress.IncrementRequests()
+	gologger.Verbose().Msgf("Sent Headless request to %s", inputURL)
+
+	reqBuilder := &strings.Builder{}
+	if request.options.Options.Debug || request.options.Options.DebugRequests {
+		gologger.Info().Msgf("[%s] Dumped Headless request for %s", request.options.TemplateID, inputURL)
+
+		for _, act := range request.Steps {
+			reqBuilder.WriteString(act.String())
+			reqBuilder.WriteString("\n")
+		}
+		gologger.Print().Msgf(reqBuilder.String())
+	}
+
+	var responseBody string
+	html, err := page.Page().Element("html")
+	if err == nil {
+		responseBody, _ = html.HTML()
+	}
+	outputEvent := request.responseToDSLMap(responseBody, reqBuilder.String(), inputURL, inputURL, page.DumpHistory())
+	for k, v := range out {
+		outputEvent[k] = v
+	}
+
+	event := eventcreator.CreateEvent(request, outputEvent, request.options.Options.Debug || request.options.Options.DebugResponse)
+
+	dumpResponse(event, request.options, responseBody, inputURL)
+
+	callback(event)
+	return nil
+}
+
+func dumpResponse(event *output.InternalWrappedEvent, requestOptions *protocols.ExecuterOptions, responseBody string, input string) {
+	cliOptions := requestOptions.Options
+	if cliOptions.Debug || cliOptions.DebugResponse {
+		highlightedResponse := responsehighlighter.Highlight(event.OperatorsResult, responseBody, cliOptions.NoColor, false)
+		gologger.Debug().Msgf("[%s] Dumped Headless response for %s\n\n%s", requestOptions.TemplateID, input, highlightedResponse)
+	}
+}
---
+++ v2/pkg/testutils/integration.go
@@ -0,0 +1,158 @@
+package testutils
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/gobwas/ws"
+	"github.com/julienschmidt/httprouter"
+)
+
+// RunNucleiTemplateAndGetResults returns a list of results for a template
+func RunNucleiTemplateAndGetResults(template, url string, debug bool, extra ...string) ([]string, error) {
+	return RunNucleiAndGetResults(true, template, url, debug, extra...)
+}
+
+// RunNucleiWorkflowAndGetResults returns a list of results for a workflow
+func RunNucleiWorkflowAndGetResults(template, url string, debug bool, extra ...string) ([]string, error) {
+	return RunNucleiAndGetResults(false, template, url, debug, extra...)
+}
+
+func RunNucleiAndGetResults(isTemplate bool, template, url string, debug bool, extra ...string) ([]string, error) {
+	var templateOrWorkflowFlag string
+	if isTemplate {
+		templateOrWorkflowFlag = "-t"
+	} else {
+		templateOrWorkflowFlag = "-w"
+	}
+
+	return RunNucleiBareArgsAndGetResults(debug, append([]string{
+		templateOrWorkflowFlag,
+		template,
+		"-target",
+		url,
+	}, extra...)...)
+}
+
+func RunNucleiBareArgsAndGetResults(debug bool, extra ...string) ([]string, error) {
+	cmd := exec.Command("./nuclei")
+	cmd.Args = append(cmd.Args, extra...)
+	if debug {
+		cmd.Args = append(cmd.Args, "-debug")
+		cmd.Stderr = os.Stderr
+		fmt.Println(cmd.String())
+	} else {
+		cmd.Args = append(cmd.Args, "-silent")
+	}
+	data, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	var parts []string
+	items := strings.Split(string(data), "\n")
+	for _, i := range items {
+		if i != "" {
+			parts = append(parts, i)
+		}
+	}
+	return parts, nil
+}
+
+var templateLoaded = regexp.MustCompile(`(?:Templates|Workflows) loaded[^:]*: (\d+)`)
+
+// RunNucleiBinaryAndGetLoadedTemplates returns a list of results for a template
+func RunNucleiBinaryAndGetLoadedTemplates(nucleiBinary string, debug bool, args []string) (string, error) {
+	cmd := exec.Command(nucleiBinary, args...)
+	if debug {
+		cmd.Args = append(cmd.Args, "-debug")
+		fmt.Println(cmd.String())
+	}
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	matches := templateLoaded.FindAllStringSubmatch(string(data), -1)
+	if len(matches) == 0 {
+		return "", errors.New("no matches found")
+	}
+	return matches[0][1], nil
+}
+
+// TestCase is a single integration test case
+type TestCase interface {
+	// Execute executes a test case and returns any errors if occurred
+	Execute(filePath string) error
+}
+
+// TCPServer creates a new tcp server that returns a response
+type TCPServer struct {
+	URL      string
+	listener net.Listener
+}
+
+// NewTCPServer creates a new TCP server from a handler
+func NewTCPServer(handler func(conn net.Conn), port ...int) *TCPServer {
+	server := &TCPServer{}
+
+	var gotPort int
+	if len(port) > 0 {
+		gotPort = port[0]
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", gotPort))
+	if err != nil {
+		panic(err)
+	}
+	server.URL = l.Addr().String()
+	server.listener = l
+
+	go func() {
+		for {
+			// Listen for an incoming connection.
+			conn, err := l.Accept()
+			if err != nil {
+				continue
+			}
+			// Handle connections in a new goroutine.
+			go handler(conn)
+		}
+	}()
+	return server
+}
+
+// Close closes the TCP server
+func (s *TCPServer) Close() {
+	s.listener.Close()
+}
+
+// NewWebsocketServer creates a new websocket server from a handler
+func NewWebsocketServer(path string, handler func(conn net.Conn), originValidate func(origin string) bool, port ...int) *httptest.Server {
+	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if value := r.Header.Get("Origin"); value != "" && !originValidate(value) {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		conn, _, _, err := ws.UpgradeHTTP(r, w)
+		if err != nil {
+			return
+		}
+		go func() {
+			defer conn.Close()
+
+			handler(conn)
+		}()
+	})
+
+	if path != "" {
+		router := httprouter.New()
+		router.HandlerFunc("*", "/test", handlerFunc)
+		return httptest.NewServer(router)
+	}
+	return httptest.NewServer(handlerFunc)
+}
